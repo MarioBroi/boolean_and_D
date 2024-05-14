0bb8afc39ad68c9f742fac4567390284844209b1
mattemutti <mattemutti@gmail.com>
2024-05-14 17:29:29 +0200

    print card - close #4

--- resources/views/guest/data.blade.php
+++ resources/views/guest/data.blade.php
@@ -1,6 +1,21 @@
 @extends('layout.app')
 @section('main-content')
-
-@dd($itemsData)
-
+    {{-- @dd($itemsData) --}}
+    <div class="container">
+        <div class="row">
+            @forelse ($itemsData as $item)
+                <div class="col">
+                    <div class="card">
+                        <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
+                        <div class="card-body">
+                            <h4 class="card-title">{{ $item['category'] }}</h4>
+                            <p class="card-text">Text</p>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <h2>Card non presente</h2>
+            @endforelse
+        </div>
+    </div>
 @endsection
