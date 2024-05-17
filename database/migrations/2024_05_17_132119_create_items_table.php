47a1c0d236f094002bf74533aa1bcc3bbf8dab51
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-17 15:26:10 +0200

    add migration with items table in database

---
+++ database/migrations/2024_05_17_132119_create_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->nullable();
+            $table->string('type')->nullable();
+            $table->string('category')->nullable();
+            $table->string('weight')->nullable();
+            $table->string('cost')->nullable();
+            $table->string('damage_dice')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
