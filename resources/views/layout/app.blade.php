6e51c4be14a68131ca84d107ea43c9420f7097c1
MarioBroi <inoix16@gmail.com>
2024-05-18 10:42:50 +0100

    add fontawesome cdn in app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,6 +7,11 @@
 
     <title>@yield('title-content', 'Boolean and Dragons')</title>
 
+    <!-- FontAwesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
