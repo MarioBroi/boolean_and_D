21c44ca855dd81b21d3125a9f4d109a1ba861283
MarioBroi <inoix16@gmail.com>
2024-05-21 14:48:24 +0100

    add breeze scaffold

--- routes/_web.php
+++ routes/_web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Guests\CharacterController;
 use App\Http\Controllers\Guests\GameController;
 use App\Http\Controllers\ItemController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,10 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Route::get('/', function () {
-    return view('welcome');
-}); */
-
 Route::get('/', [GameController::class, 'index'])->name('home');
 
 Route::get('/data', [GameController::class, 'decodeJson'])->name('data');
@@ -28,15 +23,3 @@ Route::get('/data', [GameController::class, 'decodeJson'])->name('data');
 Route::resource('/character', CharacterController::class);
 
 Route::resource('/item', ItemController::class);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
