034c8cd63b3eca08c143d4aa63d0f25776913bfa
MarioBroi <inoix16@gmail.com>
2024-05-21 18:16:06 +0100

    edit the edit page for items with correct helpers and values, delete the slug

--- resources/views/admin/items/edit.blade.php
+++ resources/views/admin/items/edit.blade.php
@@ -11,50 +11,38 @@
             <div class="mb-3">
                 <label for="" class="form-label">Name</label>
                 <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name"
-                    aria-describedby="helpId" placeholder="Inserisci il Nome del tuo oggetto" value="{{ old('name') }}" />
-                <small id="helpId" class="form-text text-muted">Help text</small>
+                    aria-describedby="helpId" value="{{ $item->name }}" />
+                <small id="helpId" class="form-text text-muted">Edit the item name</small>
                 @error('name')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
 
             <div class="mb-3">
-                <label for="" class="form-label">slug</label>
-                <input type="text" class="form-control @error('slug') is-invalid @enderror" name="slug" id="slug"
-                    aria-describedby="helpId" placeholder="Inserisci il Nome del tuo oggetto" value="{{ old('slug') }}" />
-                <small id="helpId" class="form-text text-muted">Help text</small>
-                @error('slug')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <div class="mb-3">
-                <label for="" class="form-label">type</label>
+                <label for="" class="form-label">Type</label>
                 <input type="text" class="form-control @error('type') is-invalid @enderror" name="type" id="type"
-                    aria-describedby="helpId" placeholder="Inserisci il Nome del tuo oggetto" value="{{ old('type') }}" />
-                <small id="helpId" class="form-text text-muted">Help text</small>
+                    aria-describedby="helpId" value="{{ $item->type }}" />
+                <small id="helpId" class="form-text text-muted">Edit the item type</small>
                 @error('type')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
 
             <div class="mb-3">
-                <label for="" class="form-label">category</label>
+                <label for="" class="form-label">Category</label>
                 <input type="text" class="form-control @error('category') is-invalid @enderror" name="category"
-                    id="category" aria-describedby="helpId" placeholder="Inserisci il Nome del tuo oggetto"
-                    value="{{ old('category') }}" />
-                <small id="helpId" class="form-text text-muted">Help text</small>
+                    id="category" aria-describedby="helpId" value="{{ $item->category }}" />
+                <small id="helpId" class="form-text text-muted">Edit the item category</small>
                 @error('category')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
 
             <div class="mb-3">
-                <label for="" class="form-label">weight</label>
+                <label for="" class="form-label">Weight</label>
                 <input type="text" class="form-control @error('weight') is-invalid @enderror" name="weight"
-                    id="weight" aria-describedby="helpId" placeholder="Inserisci il Nome del tuo oggetto"
-                    value="{{ old('weight') }}" />
-                <small id="helpId" class="form-text text-muted">Help text</small>
+                    id="weight" aria-describedby="helpId" value="{{ $item->weight }}" />
+                <small id="helpId" class="form-text text-muted">Edit the item weight</small>
                 @error('weight')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
@@ -62,22 +50,20 @@
 
 
             <div class="mb-3">
-                <label for="" class="form-label">cost</label>
+                <label for="" class="form-label">Cost</label>
                 <input type="text" class="form-control @error('cost') is-invalid @enderror" name="cost" id="cost"
-                    aria-describedby="helpId" placeholder="Inserisci il Nome del tuo oggetto"
-                    value="{{ old('cost') }}" />
-                <small id="helpId" class="form-text text-muted">Help text</small>
+                    aria-describedby="helpId" value="{{ $item->cost }}" />
+                <small id="helpId" class="form-text text-muted">Edit the item cost</small>
                 @error('cost')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
 
             <div class="mb-3">
-                <label for="" class="form-label">damage dice</label>
+                <label for="" class="form-label">Damage dice</label>
                 <input type="text" class="form-control @error('cost') is-invalid @enderror" name="damage_dice"
-                    id="damage_dice" aria-describedby="helpId" placeholder="Inserisci il Nome del tuo oggetto"
-                    value="{{ old('damage_dice') }}" />
-                <small id="helpId" class="form-text text-muted">Help text</small>
+                    id="damage_dice" aria-describedby="helpId" value="{{ $item->damage_dice }}" />
+                <small id="helpId" class="form-text text-muted">Edit the item dice damage</small>
                 @error('damage_dice')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
