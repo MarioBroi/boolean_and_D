807a235f79164b0b43d0a6411cd890c2ec79c632
MarioBroi <inoix16@gmail.com>
2024-05-21 21:22:42 +0100

    edit header style, add font in guest.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,80 +27,14 @@
 
 <body>
 
-    <header class="site-header">
-        <div class="container-header">
-            <div class="header-left">
-                <a href="{{ route('home') }}">
-                    <img src="https://static-00.iconduck.com/assets.00/dungeons-and-dragons-icon-512x492-vw90341y.png"
-                        alt="Boolean and Dragons Logo" class="logo">
-                </a>
-            </div>
-            <!-- /.header-left -->
-            <div class="header-middle">
-
-                <ul class="header-links">
-
-                    <li>
-                        <a class="navbar-brand" href="{{ route('home') }}">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.characters.index') }}">{{ __('Character') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.items.index') }}">{{ __('Items') }}</a>
-                    </li>
-
-                </ul>
-                <!-- /.header-links -->
-            </div>
-            <!-- /.header-middle -->
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto flex-row gap-2">
-                <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ url('admin') }}">{{ __('Dashboard') }}</a>
-                            <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-
-
-            <!-- /.header-right -->
-
-        </div>
-        <!-- /.container-header -->
-    </header>
-    <!-- /.site-header -->
+    @include('partials.header')
 
     <main>
         @yield('content')
     </main>
 
     @include('partials.footer')
+
 </body>
 
 </html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,81 +12,30 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <!-- MedievalSharp Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=MedievalSharp&display=swap" rel="stylesheet">
+
+    <!-- FontAwesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
 
-    <header class="site-header">
-        <div class="container-header">
-            <div class="header-left">
-                <a href="{{ route('home') }}">
-                    <img src="https://static-00.iconduck.com/assets.00/dungeons-and-dragons-icon-512x492-vw90341y.png"
-                        alt="Boolean and Dragons Logo" class="logo">
-                </a>
-            </div>
-            <!-- /.header-left -->
-            <div class="header-middle">
-
-                <ul class="header-links">
-                    <li>
-                        <a class="navbar-brand" href="{{ route('home') }}">Home</a>
-                    </li>
-                </ul>
-                <!-- /.header-links -->
-            </div>
-            <!-- /.header-middle -->
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto flex-row gap-2">
-                <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ url('admin') }}">{{ __('Dashboard') }}</a>
-                            <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-
-            <!-- /.header-right -->
-
-        </div>
-        <!-- /.container-header -->
-    </header>
-    <!-- /.site-header -->
-
-
-
+    @include('partials.header')
 
     <main>
-
-
         @yield('content')
-
     </main>
+
+    @include('partials.footer')
+
 </body>
 
 </html>
