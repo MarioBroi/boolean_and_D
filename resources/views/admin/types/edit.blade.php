b889a4bdbd750c16b6d1e5dbd77475ec4afa171f
MarioBroi <inoix16@gmail.com>
2024-05-29 21:58:24 +0100

    edit types edit page layout

--- resources/views/admin/types/edit.blade.php
+++ resources/views/admin/types/edit.blade.php
@@ -19,49 +19,67 @@
             </div>
             <!-- /.row -->
 
-            <div class="row py-5">
+            @include('partials.validation-errors')
+
+            <div class="row row-cols-2 py-5">
+
                 <div class="col">
-                    <div class="card ">
-                        <div class="card-body p-3 admin-text-custom better-view">
-                            <form action="{{ route('admin.types.update', $type) }}" method="post">
-                                @csrf
-                                @method('put')
 
-                                <div class="mb-3">
-                                    <label for="" class="form-label admin-text-custom">Name</label>
-                                    <input type="text" class="form-control @error('name') is-invalid @enderror"
-                                        name="name" id="name" aria-describedby="helpId"
-                                        value="{{ $type->name }}" />
-                                    <small id="helpId" class="form-text text-muted">Type the type name</small>
-                                    @error('name')
-                                        <div class="alert alert-danger">{{ $message }}</div>
-                                    @enderror
-                                </div>
+                    <form action="{{ route('admin.types.update', $type) }}" method="post" class="better-view">
+                        @csrf
+                        @method('put')
 
-                                <div class="mb-3">
-                                    <label for="desc" class="form-label admin-text-custom">Description</label>
-                                    <input type="text" class="form-control @error('desc') is-invalid @enderror"
-                                        name="desc" id="desc" aria-describedby="helpId"
-                                        value="{{ $type->desc }}" />
-                                    <small id="helpId" class="form-text text-muted">Type the type description</small>
-                                    @error('desc')
-                                        <div class="alert alert-danger">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                <button type="submit" class="card-buttons-custom fs-5">
-                                    Update
-                                </button>
-
-                            </form>
+                        <div class="mb-3">
+                            <label for="" class="form-label fw-bold admin-text-custom">Name</label>
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                                id="name" aria-describedby="helpId" value="{{ $type->name }}" />
+                            <small id="helpId" class="form-text text-muted">Type the type name</small>
+                            @error('name')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
-                        <!-- /.card-body -->
-                    </div>
-                    <!-- /.card -->
+                        <!-- /.name -->
+
+                    </form>
+
                 </div>
                 <!-- /.col -->
+
+                <div class="col">
+
+                    <form action="{{ route('admin.types.update', $type) }}" method="post" class="better-view">
+                        @csrf
+                        @method('put')
+
+                        <div class="mb-3">
+                            <label for="desc" class="form-label fw-bold admin-text-custom">Description</label>
+                            <textarea class="form-control character-text" name="desc" id="desc" @error('desc') is-invalid @enderror
+                                aria-describedby="descHelper" placeholder="..." style="height: 200px">{{ $type->desc }}</textarea>
+                            <small id="helpId" class="form-text text-muted">Type the type description</small>
+                            @error('desc')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <!-- /.description -->
+
+                    </form>
+
+                </div>
+                <!-- /.col -->
+
             </div>
             <!-- /.row -->
+
+            <form action="{{ route('admin.types.update', $type) }}" method="post" class="text-center">
+                @csrf
+                @method('put')
+
+                <button type="submit" class="card-buttons-custom mb-5 px-5 py-3 fw-bold">
+                    Update
+                </button>
+
+            </form>
+
         </div>
         <!-- /.container -->
     </div>
