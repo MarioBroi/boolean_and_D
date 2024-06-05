bba0ee3eefa77ee0ed354ffb39fac977b91123d7
mattemutti <mattemutti@gmail.com>
2024-06-05 17:03:07 +0200

    add route Api characters

---
+++ app/Http/Controllers/API/CharacterController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Character;
+
+class CharacterController extends Controller
+{
+    public function index()
+    {
+        $characters = Character::with('type', 'items')->orderbyDesc('id')->paginate();
+        return response()->json([
+            'success' => true,
+            'characters' => $characters,
+        ]);
+    }
+
+    public function show($id)
+    {
+        $character = Character::with('type', 'items')->where('id', $id)->first();
+        if ($character) {
+            return response()->json([
+                'success' => true,
+                'response' => $character,
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'response' => 'Error 404',
+            ]);
+        }
+
+    }
+
+
+}
